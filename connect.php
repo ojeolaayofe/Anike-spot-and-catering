3701489cc4d1b0448a53aeb5b73b401b74ede212
adeola541 <80450008+adeola541@users.noreply.github.com>
2021-03-29 21:23:45 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+    $fristName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password2 = $_POST['password'];
+    $number = $_POST['number'];
+
+    //Database connection
+?>
